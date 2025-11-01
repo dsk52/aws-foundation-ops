7e4934cc76cd74f4c0d270e3c86cd70369908b13
Daisuke KONISHI <skydai1151@gmail.com>
2025-11-01 17:00:36 +0900

    feat: ヘルスチェックを通知する仕組みを追加

--- bin/foundation-ops.ts
+++ bin/foundation-ops.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { BudgetNotificationStack } from '../lib/BudgetNotificationStack';
 import { ChatbotStack } from '../lib/ChatbotStack';
+import { HealthCheckNotificationStack } from '../lib/HealthCheckNotificationStack';
 
 const app = new cdk.App();
 const env = { region: 'ap-northeast-1' };
@@ -11,6 +12,11 @@ const chatbotStack = new ChatbotStack(app, 'ChatbotStack', {
 });
 
 new BudgetNotificationStack(app, 'BudgetNotificationStack', {
+  env,
+  slackChannel: chatbotStack.slackChannel,
+});
+
+new HealthCheckNotificationStack(app, 'HealthCheckNotificationStack', {
   env,
   slackChannel: chatbotStack.slackChannel,
 });
