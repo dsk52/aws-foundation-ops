bc04c2faeda79adbee7a32e783c01fa22e5e28fa
Daisuke KONISHI <skydai1151@gmail.com>
2026-01-25 18:17:36 +0900

    Node関連ファイル削除とTerraform検証に整理

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-};
